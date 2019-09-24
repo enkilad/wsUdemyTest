f878dc11f69d0264085882888c6a71a93663a780
Enkilad <naran_98@mail.ru>
2019-09-24 16:06:04 +0300

    the end

---
+++ translate/src/components/LanguageSelector.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class LanguageSelector extends React.Component {
+  static contextType = LanguageContext;
+
+  render() {
+    return (
+      <div>
+        <span>Select a language: </span>
+        <i
+          className="flag us"
+          onClick={() => this.context.onLanguageChange('english')}
+        />
+        <i
+          className="flag nl"
+          onClick={() => this.context.onLanguageChange('dutch')}
+        />
+      </div>
+    );
+  }
+}
+
+export default LanguageSelector;
